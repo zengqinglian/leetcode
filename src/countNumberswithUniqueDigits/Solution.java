833bd7dca0d0edc70d246f6f2b67b74efb256180
Eric Zeng <eric.zeng@capco.com>
2016-08-02 18:19:38 +0100

    [leetcode] math solution - 1ms

--- src/countNumberswithUniqueDigits/Solution.java
+++ src/countNumberswithUniqueDigits/Solution.java
@@ -4,34 +4,39 @@ public class Solution
 {
     // math solution
     public int countNumbersWithUniqueDigits( int n ) {
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        if (n == 0) {
+            return 1;
+        }
+        if(n==1){
+            return 10;
+        }
+        dp[1] = 10;
         int total = 0;
-        for( int i = 2; i <= n; i++ ) {
-            total += countNumberWithNUniqueDigits( n, i ) * calculateCombinations( n, i );
+        int digits = 2;
+        while(digits<=n){
+            total = 0;
+            for (int i = 1; i <= digits; i++) {
+                if(i==1){
+                    total=9;
+                } else if (i == 2) {
+                    total *= 9;
+                } else {
+                    total *= (9 - i + 2);
+                }
+            }
+            dp[digits] = dp[digits - 1] + total;
+            digits++;
         }
+        
+        return dp[n];
 
-        return (int) Math.pow( 10, n ) - total;
     }
 
-    private int countNumberWithNUniqueDigits( int totalDigits, int N ) {
-        return 9 * (factoria( 9 ) / factoria( 9 - N ));
-    }
-
-    private int calculateCombinations( int totalDigits, int N ) {
-        return factoria( totalDigits ) / (factoria( N ) * factoria( totalDigits - N ));
-    }
-
-    private int factoria( int n ) {
-        if( n == 0 ) {
-            return 1;
-        }
-        if( n == 1 ) {
-            return 1;
-        }
-
-        return n * factoria( n - 1 );
-    }
 
     public static void main( String[] args ) {
-
+        Solution s = new Solution();
+        s.countNumbersWithUniqueDigits(3);
     }
 }
