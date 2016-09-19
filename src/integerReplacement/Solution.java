aa663cee81a7a91c1a50b5c9536d4549c4731ab6
Eric Zeng <eric.zeng@capco.com>
2016-09-19 15:33:18 +0100

    [leetcode] integer replacement solution - recurssive

---
+++ src/integerReplacement/Solution.java
@@ -0,0 +1,15 @@
+package integerReplacement;
+
+public class Solution {
+    public int integerReplacement(int n) {
+        if (n == 1) {
+            return 0;
+        }
+
+        if (n % 2 == 0) {
+            return 1 + integerReplacement(n / 2);
+        } else {
+            return 1 + Math.min(integerReplacement(n + 1), integerReplacement(n - 1));
+        }
+    }
+}
