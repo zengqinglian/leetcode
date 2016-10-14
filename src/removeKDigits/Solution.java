67f2d540be6fd79bd47fc54555e9987a98c94dee
Eric Zeng <eric.zeng@capco.com>
2016-10-14 16:43:53 +0100

    [leetcode] one my slow solution and someone quick solution.

---
+++ src/removeKDigits/Solution.java
@@ -0,0 +1,18 @@
+package removeKDigits;
+
+public class Solution {
+
+    // 110ms solution - slow beat 15% only
+    public String removeKdigits(String num, int k) {
+        int length = num.length();
+
+        while (k > 0) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.removeKdigits("10200", 1);
+    }
+}
