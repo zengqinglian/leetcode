311e90317e668730efa15cd488fd3784655c219b
zengqinglian <zengqinglian@gmail.com>
2017-07-20 20:33:48 +0100

    leetcode - two good solution beat over 50%

---
+++ src/combinationSumII/Solution.java
@@ -0,0 +1,44 @@
+package combinationSumII;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+  // back track to find out all combination. - important step is
+  // tempList.remove. remove the add last added element.
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    List<List<Integer>> list = new LinkedList<List<Integer>>();
+    Arrays.sort(candidates);
+    backtrack(list, new ArrayList<Integer>(), candidates, target, 0);
+    return list;
+  }
+
+  private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] cand, int remain,
+      int start) {
+
+    if (remain < 0)
+      return;
+    /** no solution */
+    else if (remain == 0)
+      list.add(new ArrayList<>(tempList));
+    else {
+      for (int i = start; i < cand.length; i++) {
+        if (i > start && cand[i] == cand[i - 1])
+          continue;
+        /** skip duplicates */
+        tempList.add(cand[i]);
+        backtrack(list, tempList, cand, remain - cand[i], i + 1);
+        tempList.remove(tempList.size() - 1);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    int[] candidates = {10, 1, 2, 7, 6, 1, 5};
+
+    s.combinationSum2(candidates, 8);
+  }
+}
---
+++ src/fourSumII/Solution.java
@@ -0,0 +1,34 @@
+package fourSumII;
+
+import java.util.HashMap;
+
+public class Solution {
+  // beat 70% solution
+  public int fourSumCount(int[] A, int[] B, int[] C, int[] D) {
+
+    Map<Integer, Integer> map = new HashMap<>();
+
+    for (int i = 0; i < C.length; i++) {
+      for (int j = 0; j < D.length; j++) {
+        int sum = C[i] + D[j];
+        map.put(sum, map.getOrDefault(sum, 0) + 1);
+      }
+    }
+
+    int res = 0;
+    for (int i = 0; i < A.length; i++) {
+      for (int j = 0; j < B.length; j++) {
+        res += map.getOrDefault(-1 * (A[i] + B[j]), 0);
+      }
+    }
+
+    return res;
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    int[] A = {1, 2, -2}, B = {-2, -1, 1}, C = {-1, 2, 1}, D = {0, 2, 0};
+    s.fourSumCount(A, B, C, D);
+  }
+
+}
