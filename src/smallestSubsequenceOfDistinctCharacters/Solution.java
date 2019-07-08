7e963f3e11fcaefa02e041dd6e5221728d1168a6
Qinglian Zeng <zengqinglian@hotmail.com>
2019-07-08 11:33:35 +0100

    Update Solution.java

--- src/smallestSubsequenceOfDistinctCharacters/Solution.java
+++ src/smallestSubsequenceOfDistinctCharacters/Solution.java
@@ -1,40 +1,60 @@
 package smallestSubsequenceOfDistinctCharacters;
 
-public class Solution2 {
-    // someone's 2 ms solution, beat 90%
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    // overtime limit solution - 14/65
+    String res = null;
     public String smallestSubsequence(String text) {
-	int[] count = new int[26];
-	for (int i = 0; i < text.length(); i++) {
-	    char c = text.charAt(i);
-	    count[c - 'a']++;
+	Set<Character> set = new HashSet<>();
+	for (char c : text.toCharArray()) {
+	    set.add(c);
 	}
 
-	boolean[] used = new boolean[26];
-	StringBuilder sb = new StringBuilder();
-	for (int i = 0; i < text.length(); i++) {
-	    char c = text.charAt(i);
-	    if (used[c - 'a']) {
-		count[c - 'a']--;
-		continue;
-	    }
-	    int last = sb.length() - 1;
-	    while (sb.length() > 0 && c < sb.charAt(last) && count[sb.charAt(last) - 'a'] > 0) {
-		used[sb.charAt(last) - 'a'] = false;
-		sb.deleteCharAt(last);
-		last -= 1;
-	    }
-	    used[c - 'a'] = true;
-	    sb.append(c);
-	    count[c - 'a']--;
+	int total = set.size();
+	if (total == 1) {
+	    return text.substring(0, 1);
 	}
-
-	return sb.toString();
 	
+	for (int i = 0; i <= text.length() - total + 1; i++) {
+	    getString(i, text, total, new HashSet<Character>(), new StringBuilder());
+	}
+
+	return res;
+
+    }
+
+
+    private void getString(int i, String text, int total, HashSet<Character> set, StringBuilder sb) {
+	char c = text.charAt(i);
+	if (set.add(c)) {
+	    sb.append(c);
+	    if (set.size() == total) {
+		if (res == null) {
+		    res = sb.toString();
+		} else {
+		    if (res.compareTo(sb.toString()) > 0) {
+			res = sb.toString();
+		    }
+		}
+	    } else {
+		for (int j = i + 1; j < text.length() - total + set.size() + 1; j++) {
+		    getString(j, text, total, set, sb);
+		}
+	    }
+	    set.remove(c);
+	    sb.setLength(sb.length() - 1);
+	} else {
+	    for (int j = i + 1; j < text.length() - total + set.size() + 1; j++) {
+		getString(j, text, total, set, sb);
+	    }
+	}
     }
 
     public static void main(String[] args) {
-	Solution2 s = new Solution2();
-	s.smallestSubsequence("leetcode");
-    }
+	Solution s = new Solution();
+	s.smallestSubsequence("ecbacba");
 
+    }
 }
