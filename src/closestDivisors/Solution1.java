25b616c8705c3ad341b05e1a04f3a975dd46fba6
Qinglian Zeng <zengqinglian@hotmail.com>
2021-02-01 14:55:55 +0000

    Create Solution1.java

---
+++ src/closestDivisors/Solution1.java
@@ -0,0 +1,21 @@
+package closestDivisors;
+
+public class Solution1 {
+    //someone's 7ms solution, beat 70%
+    public int[] closestDivisors(int num) {
+        int[] h = new int[2];
+        for (int i = (int) Math.sqrt(num + 2); i >= 1; i--) {
+            if ((num + 1) % i == 0){
+                h[0] = i;
+                h[1] = (num + 1) / i;
+                break;
+            }
+            if ((num + 2) % i == 0){
+                h[0] = i;
+                h[1] = (num + 2) / i;
+                break;
+            }
+        }
+        return h;
+    }
+}
