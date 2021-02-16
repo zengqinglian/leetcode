b21d740da0259a0e655a9be18599867b4e57fc04
Qinglian Zeng <zengqinglian@hotmail.com>
2021-02-16 13:58:50 +0000

    Create Solution1.java

---
+++ src/longestZigzagPathInaBinaryTree/Solution1.java
@@ -0,0 +1,59 @@
+package longestZigzagPathInaBinaryTree;
+
+//6ms solution, beat 50%
+public class Solution1 {
+    int res = 0;
+    public int longestZigZag(TreeNode root) {
+        if(root.left == null && root.right==null){
+            res = Math.max(res, 0);
+        }
+        if(root.left!=null){
+            res = Math.max(res, 1 + longestZigZag(root.left, -1));
+            longestZigZag(root.left);
+        }
+        if(root.right!=null){
+            res = Math.max(res, 1 + longestZigZag(root.right, 1));
+            longestZigZag(root.right);
+        }
+        return res;
+    }
+
+    public int longestZigZag(TreeNode root, int index) {
+        if(root.val ==0){
+            return 0;
+        }
+        root.val = 0;
+        if(index < 0){
+            if(root.right == null){
+                return 0;
+            }else{
+                return 1 + longestZigZag(root.right, -index);
+            }
+        }else{
+            if(root.left == null){
+               return 0;
+            }else{
+                return 1 + longestZigZag(root.left, -index);
+            }
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
